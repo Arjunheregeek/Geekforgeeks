8b6fc899d56ad9ec1b0a4aebad115cba65702d4a
Arjun Gupta <148304957+Arjunheregeek@users.noreply.github.com>
2025-01-28 21:11:10 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Permutations of a String/permutations-of-a-string.py
@@ -0,0 +1,28 @@
+#User function Template for python3
+from itertools import permutations
+
+class Solution:
+    def findPermutation(self, s):
+        return list(set("".join(i) for i in permutations(s)))
+        # Code here
+        
+
+
+
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+
+if __name__ == '__main__':
+    t = int(input())
+    for i in range(t):
+        S = input()
+        ob = Solution()
+        ans = ob.findPermutation(S)
+        ans.sort()
+        for i in ans:
+            print(i, end=" ")
+        print()
+        print("~")
+
+# } Driver Code Ends
